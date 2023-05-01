fbe9280c155fbd3f16fb2b79a9517a1d6e00ca42
sajalkhan <sajalhossen@yahoo.com>
2023-05-01 17:10:54 +0600

    implement base quese init setup

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Application } from 'express';
 import { authRoutes } from '@auth/routes/authRoutes';
+import { serverAdapter } from '@service/queues/base.queue';
 
 const BASE_PATH = '/api/v1';
 
 export default (app: Application) => {
   const routes = () => {
+    app.use('/queues', serverAdapter.getRouter()); // this is only for check GUI of MQ
     app.use(BASE_PATH, authRoutes.getRouter());
   };
   routes();
---
+++ src/shared/services/queues/base.queue.ts
@@ -0,0 +1,50 @@
+import Logger from 'bunyan';
+import Queue, { Job } from 'bull';
+import { config } from '@root/config';
+import { createBullBoard } from '@bull-board/api';
+import { ExpressAdapter } from '@bull-board/express';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
+
+let bullAdapters: BullAdapter[] = [];
+export let serverAdapter: ExpressAdapter;
+
+export abstract class BaseQueue {
+  queue: Queue.Queue;
+  log: Logger;
+
+  constructor(queueName: string) {
+    this.queue = new Queue(queueName, `${config.REDIS_HOST}`);
+    bullAdapters.push(new BullAdapter(this.queue));
+    bullAdapters = [...new Set(bullAdapters)];
+    serverAdapter = new ExpressAdapter();
+    serverAdapter.setBasePath('/queues');
+
+    createBullBoard({
+      queues: bullAdapters,
+      serverAdapter
+    });
+
+    this.log = config.createLogger(`${queueName}Queue`);
+
+    this.queue.on('completed', (job: Job) => {
+      job.remove();
+    });
+
+    this.queue.on('global:completed', (jobId: string) => {
+      this.log.info(`Job ${jobId} completed`);
+    });
+
+    this.queue.on('global:stalled', (jobId: string) => {
+      this.log.info(`Job ${jobId} is stalled`);
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  protected addJob(name: string, data: any): void {
+    this.queue.add(name, data, { attempts: 3, backoff: { type: 'fixed', delay: 5000 } });
+  }
+
+  protected processJob(name: string, concurrency: number, callback: Queue.ProcessCallbackFunction<void>): void {
+    this.queue.process(name, concurrency, callback);
+  }
+}

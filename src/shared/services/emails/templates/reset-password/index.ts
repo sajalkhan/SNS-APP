0e0c89cb8330dfa63b7d2ab53e0305f89fd541ce
sajalkhan <sajalhossen@yahoo.com>
2023-05-06 23:55:21 +0600

    implement email template

---
+++ src/shared/services/emails/templates/reset-password/index.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import { load } from 'cheerio';
+import { IResetPasswordParams } from '@user/interfaces/user.interface';
+
+class ResetPasswordTemplate {
+  public passwordResetConfirmationTemplate(templateParams: IResetPasswordParams): string {
+    const { username, email, ipaddress, date } = templateParams;
+    // Load index.html and parse it using Cheerio
+    const html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+    const $ = load(html);
+
+    // Update the username and reset link values in the HTML template
+    $('.reset-password__userName').text(username);
+    $('.reset-password__email').text(email);
+    $('.reset-password__ip').text(ipaddress);
+    $('.reset-password__date').text(date);
+
+    // Return the updated HTML content as a string
+    return $.html();
+    return '';
+  }
+}
+
+export const resetPasswordTemplate: ResetPasswordTemplate = new ResetPasswordTemplate();

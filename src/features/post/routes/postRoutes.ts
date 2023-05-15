b147ba493ee62e8197db1b476316dbbd660763e8
sajalkhan <sajalhossen@yahoo.com>
2023-05-15 22:20:01 +0600

    implement update post api

--- src/features/post/routes/postRoutes.ts
+++ src/features/post/routes/postRoutes.ts
@@ -1,5 +1,6 @@
 import { Get } from '@post/controllers/get-post';
 import { Create } from '@post/controllers/create-post';
+import { Update } from '@post/controllers/update-post';
 import { Delete } from '@post/controllers/delete-post';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import express, { Router } from 'express';
@@ -18,6 +19,9 @@ class PostRoutes {
 
     this.router.post('/post', authMiddleware.checkAuthentication, Create.prototype.post);
     this.router.post('/post/image/post', authMiddleware.checkAuthentication, Create.prototype.postWithImage);
+
+    this.router.put('/post/:postId', authMiddleware.checkAuthentication, Update.prototype.posts);
+    this.router.put('/post/image/:postId', authMiddleware.checkAuthentication, Update.prototype.postWithImage);
 
     this.router.delete('/post/:postId', authMiddleware.checkAuthentication, Delete.prototype.post);
   }
